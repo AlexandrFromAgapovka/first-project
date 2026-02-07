c6a8b49b3cd206ffcaa03ea2473daf9eefdff994
AlexandrFromAgapovka <Agapo4Aleksandr@yandex.ru>
2026-02-07 20:51:16 +0300

    Add .env support and print_author function using python-dotenv

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello from repository!') 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()           # загружаем переменные из .env
+
+def print_author():
+    author = os.getenv("AUTHOR")    # читаем значение переменной AUTHOR
+    print(f"Автор проекта: {author}")
+
+print_author()
